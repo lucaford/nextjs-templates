8adeccd215d7489149570906a62529c2ae3f4889
Luca Ford <lford@BA-MAC-123.local>
2024-05-16 17:26:08 +0200

    add 'importar aqui' to page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,8 @@
-import { Footer } from "@/sections/footer/Footer";
-import { FooterMinimalist } from "@/sections/footer/FooterMinimalist";
-import { SimpleWithBackground } from "@/sections/hero/SimpleWithBackground";
-import SimpleWithoutBackground from "@/sections/hero/SimpleWithoutBackground";
-import { LeadMailList } from "@/sections/leadGen/LeadMailList";
-import { SimplePricingWithBackground } from "@/sections/pricing/SimplePricingWithBackground";
-import { TitleWithImages } from "@/sections/showcase/TitleWithImages";
-
 export default function Home() {
   return (
     <main>
-      <SimpleWithBackground />
-      <SimplePricingWithBackground />
-      <TitleWithImages />
-      <LeadMailList />
-      <Footer />
+      {/* {importar aqui} */}
+      {/* {importar aqui} */}
     </main>
   );
 }
