2163c55c3b844ed9b00007b5bd2c616c69ba0c2f
Luca Ford <lford@BA-MAC-123.local>
2024-05-16 17:16:06 +0200

    Add index.ts file in sections

---
+++ src/sections/index.ts
@@ -0,0 +1,12 @@
+export { Footer } from "./footer/Footer";
+export { FooterMinimalist } from "./footer/FooterMinimalist";
+
+export { SimpleWithBackground } from "./hero/SimpleWithBackground";
+export { default as SimpleWithoutBackground } from "./hero/SimpleWithoutBackground";
+
+export { LeadMailList } from "./leadGen/LeadMail";
+
+export { SimplePricing } from "./pricing/SimplePricing";
+export { SimplePricingWithBackground } from "./pricing/SimplePricingWithBackground";
+
+export { TitleWithImages } from "./showcase/TitleWithImages";
