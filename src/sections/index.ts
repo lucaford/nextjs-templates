5c4bc83f6f34f0268075b19c3dd15b68313ba8b9
Luca Ford <lford@BA-MAC-123.local>
2024-06-07 11:58:39 -0300

    Add discover and courses

--- src/sections/index.ts
+++ src/sections/index.ts
@@ -1,6 +1,11 @@
 export { Footer } from "./footer/Footer";
 export { FooterMinimalist } from "./footer/FooterMinimalist";
 
+export { CourseWithCards } from "./courses/CourseWithCards";
+
+export { DiscoverWithBulletPoints } from "./discover/DiscoverWithBulletPoints";
+export { DiscoverWithCheckPoints } from "./discover/DiscoverWithCheckPoints";
+
 export { SimpleWithBackground } from "./hero/SimpleWithBackground";
 export { default as SimpleWithoutBackground } from "./hero/SimpleWithoutBackground";
 
